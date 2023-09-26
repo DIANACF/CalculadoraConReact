70956df294bb7ccaada39f0a6fefada223844e92
DIANACF <djcarranza@uniboyaca.edu.co>
2023-09-25 20:50:33 -0500

    Se realizó la lógica del prorama en page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,70 +1,103 @@
 "use client";
 
 import { useState } from 'react';
+import './page.css'
 
-
- let Num1
- let Num2
-
-function  Suma(Num1, Num2){
-  return (Num1 + Num2);
-}
-
-function Resta(Num1, Num2){
-  return (Num1 - Num2);
-}
-
-function Mult (Num1, Num2){
-  return (Num1 * Num2);
-}
-
-function Dividir (Num1, Num2){
-  return (Num1/Num2);
-}
-
-import Image from 'next/image'
-import styles from './page.module.css'
 
 export default function Home() {
+
+  const [num,setNum] = useState(0);
+  const [num2,setNum2] = useState(0);
+  const [operacion,setOperacion] = useState();
+
+  function PonerNumero(e){
+    let Pnumero = e.target.value
+
+    if(num===0){
+
+      setNum(Pnumero);
+    }
+    else{
+      setNum(num + Pnumero)
+    }
+    
   
+  }
+
+  function porcentaje (){
+  setNum(num/100);
+  }
+
+  function operaciones(e){
+    let oper = e.target.value
+    setOperacion(oper);
+    setNum2(num);
+    setNum(0);
+
+  }
+
+  function calcular (){
+    
+    if (operacion === "+"){
+
+      setNum(parseFloat(num2) + parseFloat(num));
+
+    }else if (operacion === "-"){
+
+      setNum(parseFloat(num2) - parseFloat(num));
+
+    }else if(operacion === "x"){
+
+      setNum(parseFloat(num2) * parseFloat(num));
+
+    }else if(operacion ==="/"){
+      setNum(parseFloat(num2) / parseFloat(num));
+
+    }
+  }
+
+  function limpiar (){
+    setNum(0)
+  }
+
  return (
   <>
-  <input placeholder='Ingrese La Operación'></input>
+  <h3 class= "MuestraResultado">{num}</h3>
   <section class="botones" >
+   
+
     <div>
-      <button>0</button>
+      <button onClick={PonerNumero} value={7}>7</button>
+      <button onClick={PonerNumero} value={8}>8</button>
+      <button onClick={PonerNumero} value={9}>9</button>
+    </div>
+      
+      
+    <div>
+      <button onClick={PonerNumero} value={4}>4</button>
+      <button onClick={PonerNumero} value={5}>5</button>
+      <button onClick={PonerNumero} value={6}>6</button>
     </div>
 
     <div>
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
+      <button onClick={PonerNumero} value={1}>1</button>
+      <button onClick={PonerNumero} value={2}>2</button>
+      <button onClick={PonerNumero} value={3}>3</button>
     </div>
-      
-      
     <div>
-      <button>4</button>
-      <button>5</button>
-      <button>6</button>
-    </div>
-
-    <div>
-      <button>7</button>
-      <button>8</button>
-      <button>9</button>
-    </div>
-      
+      <button onClick={PonerNumero} value={0}>0</button>
+    </div>  
   </section>
 
-  <section class="operaciones">
+  <section class="BotonesOperaciones">
     <div>
-      <button>C</button>
-      <button>%</button>
-      <button>+</button>
-      <button>-</button>
-      <button>/</button>
-      <button>x</button>
-      <button>=</button>
+      <button onClick={limpiar}>AC</button>
+      <button onClick={porcentaje}>%</button>
+      <button onClick={operaciones} value="+">+</button>
+      <button onClick={operaciones} value="-">-</button>
+      <button onClick={operaciones} value="/">/</button>
+      <button onClick={operaciones} value="x">x</button>
+      <button onClick={calcular}>=</button>
 
     </div>
 
